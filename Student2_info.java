25345df6ffab21611ff2577559c7f4870a138737
Ankita <01fe22bca021@kletech.ac.in>
2024-03-10 22:51:27 +0530

    code for student

---
+++ Student2_info.java
@@ -0,0 +1,23 @@
+public class Student2_info{
+    String name="Ranjita.H.Illur";
+    String department="BCA";
+    String program="BCA";
+    public String RegistertedCourse[]={" ","C#","Applied statistics","Java"};
+
+    public void display(){
+        System.out.println("----Student Details----");
+        System.out.println("Name:"+name);
+        System.out.println("Department: "+department);
+        System.out.println("Program:"+program);
+        System.out.print("Registered Courses:\n");
+        for(int i=1;i<=3;i++){
+            System.out.println(i+":"+RegistertedCourse[i]);
+        }
+    }
+
+    public static void main(String[] args){
+        Student2_info s = new Student2_info();
+        s.display();
+       
+    }
+}
